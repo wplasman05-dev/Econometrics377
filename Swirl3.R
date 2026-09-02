07548e41ca584507cc78c965567ad8f949de4135
wplasman05-dev <wplasman05@gmail.com>
2026-09-02 14:32:04 -0400

    Add files via upload

---
+++ Swirl3.R
@@ -0,0 +1,27 @@
+swirl()
+plasmanw
+1
+3
+120
+1:20
+pi:10
+15:1
+?:
+?':'
+?':'.
+?`:`.
+?`:`
+seq(1,20)
+seq(10,0.5)
+seq(0,10,by = 0.5)
+seq(5,10,lenth=30)
+my_seq <- seq(5,10,length=30)
+length()
+length(my_seq)
+1:length(my_seq)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(0,times=40)
+rep(c(0,1,2),times=10)
+rep(c(0,1,2),each = 10)
+1
