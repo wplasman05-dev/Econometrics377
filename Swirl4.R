14cded5ac7c74455f9c71d6ca322240e1430f5c4
wplasman05-dev <wplasman05@gmail.com>
2026-09-09 17:37:09 -0400

    Add files via upload

---
+++ Swirl4.R
@@ -0,0 +1,30 @@
+library(swirl)
+swirl()
+plasmanw
+1
+4
+num_vect <- c(.5,55,-10, 6)
+#assigning a logical vector statement to the recently created "num_vector"
+tf <- num_vect<1
+#creating a new logical vector
+num_vect >= 6
+#true/false questions
+2
+1
+2
+2
+#character 
+my_char <- c("My","name","is")
+my_char
+#paste function
+paste(my_char, collapse = " ")
+#character vectors
+c(my_char, "will_plasman")
+my_name <- c(my_char, "will_plasman")
+my_name
+paste(my_name, collapse = " ")
+#more paste functions
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+2
